0cde02f901900d639a5c379ac525b022f7a7d8f4
THO-Gaetan <9771@holbertonstudents.com>
2024-12-19 10:51:10 +0100

    starting the shell.c script

--- shell.c
+++ shell.c
@@ -1,2 +1,17 @@
 #include "main.h"
-getline
+
+#define MAX_CMD_LENGHT 1024
+
+int shell(void)
+{
+	char *box;
+	size_t n = MAX_CMD_LENGHT;
+
+	while (strcmp(box, "exit") != 0)
+	{
+		printf("Ardo@Gaetan:My_Shell$ ");
+		getline(&box, &n, stdin);
+		printf("%s", box);
+	}
+	return (0);
+}
